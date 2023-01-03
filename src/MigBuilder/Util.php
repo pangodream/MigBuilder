f9d35953a3a48f35bf5a0414e3bf87a0850caba0
Paulo Henrique Garcia <paulohenriquesg@gmail.com>
2023-01-03 21:59:36 +0100

    Refactor to use Laravel Str helper class

--- src/MigBuilder/Util.php
+++ src/MigBuilder/Util.php
@@ -1,33 +1,14 @@
 <?php
-/**
- * Date: 20/12/2021
- * Time: 19:38
- */
 
 namespace MigBuilder;
 
+use Illuminate\Support\Str;
 
 class Util
 {
     public static function firstUpper($name, $evenFirstOne = true){
-        $resName = "";
-        $ucase = $evenFirstOne;
-        $skip = false;
-        for($i=0;$i<strlen($name);$i++){
-            $l= strtolower(substr($name, $i, 1));
-            if($l == "_"){
-                $skip = true;
-                $ucase = true;
-            }else{
-                if($ucase == true){
-                    $l = strtoupper($l);
-                    $ucase = false;
-                }
-                $resName .= $l;
-                $skip = false;
-            }
-
-        }
-        return $resName;
+        $name = (stripos($name, '_') === false) ? Str::snake($name) : $name;
+        
+        return ucfirst(Str::of($name)->camel());
     }
 }
