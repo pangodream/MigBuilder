08dc492061d029e2e9680543d0888e18b5dffb5b
Paulo Henrique Garcia <paulohenriquesg@gmail.com>
2023-01-06 21:57:04 +0100

    Reintroduce $evenFirstOne logic

--- src/MigBuilder/Util.php
+++ src/MigBuilder/Util.php
@@ -8,7 +8,13 @@ class Util
 {
     public static function firstUpper($name, $evenFirstOne = true){
         $name = (stripos($name, '_') === false) ? Str::snake($name) : $name;
+
+        $processedName = Str::of($name)->camel();
+
+        if ($evenFirstOne === false) {
+            return $processedName;
+        }
         
-        return ucfirst(Str::of($name)->camel());
+        return ucfirst($processedName);
     }
 }
